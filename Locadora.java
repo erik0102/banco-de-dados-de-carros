639dfdf1453e3394efeb6507e0739d90c2df52b0
Erik <erikbortoluzi.cabralbc@gmail.com>
2025-09-29 19:34:33 -0300

    Add files via upload

---
+++ Locadora.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.erik.locadora;
+
+/**
+ *
+ * @author 08307
+ */
+public class Locadora {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
